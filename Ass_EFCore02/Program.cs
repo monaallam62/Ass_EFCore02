ba0089fd2d6339db2f6c695294edd46dc1f98cdb
monaallam62 <monaallam5102@gmail.com>
2026-04-25 15:27:11 +0300

    Add project files.

---
+++ Ass_EFCore02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ass_EFCore02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
