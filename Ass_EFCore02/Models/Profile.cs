cc2fa046d47451cce72eb9610e88ee2971cdee51
monaallam62 <monaallam5102@gmail.com>
2026-04-25 22:27:51 +0300

    Profile

---
+++ Ass_EFCore02/Models/Profile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass_EFCore02.Models
+{
+    public class Profile
+    {
+        public int ProfileId { get; set; }
+        public string Bio { get; set; } 
+        public string Website { get; set; }
+        public string Logo { get; set; }
+
+        //Navigation Property 
+        public Organizer Organizer{ get; set; }
+
+        //FK
+        public int OrganizerId { get; set; }
+    }
+}
