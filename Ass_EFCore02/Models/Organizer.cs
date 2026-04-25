c6f2128537545b2d5b8db1c1452aed1e965f038a
monaallam62 <monaallam5102@gmail.com>
2026-04-25 22:28:06 +0300

    Organizer

---
+++ Ass_EFCore02/Models/Organizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass_EFCore02.Models
+{
+    public class Organizer
+    {
+        public int OrganizerId { get; set; }
+        public string Name { get; set; }
+        public string? CompanyName { get; set; }
+
+        //Navigation Property
+        public Profile Profilee { get; set; }
+
+    }
+}
